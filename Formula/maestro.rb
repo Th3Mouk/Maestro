ccc9f9babdf3cd5fb073c5373e88566a97f1de9e
Jérémy <marodon.jeremy@gmail.com>
2026-04-01 17:04:02 +0200

    chore(homebrew): update formula for v0.1.5 (#25)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/maestro.rb
+++ Formula/maestro.rb
@@ -1,8 +1,8 @@
 class Maestro < Formula
   desc "Multi-repository workspaces for engineering teams"
   homepage "https://github.com/Th3Mouk/maestro"
-  url "https://registry.npmjs.org/@th3mouk/maestro/-/maestro-0.1.4.tgz"
-  sha256 "8d2f4e0a56a0ec0cb3c26d6873568ed4d290c7d1ddf94e2e28de283c5db491fa"
+  url "https://registry.npmjs.org/@th3mouk/maestro/-/maestro-0.1.5.tgz"
+  sha256 "0147ce24753fdd3008686c15378538fb0ea2c2004e9c4ed5a12339943981e49b"
   license "MIT"
 
   depends_on "node"
